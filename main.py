2f6a20d6d4d1ba296e26972b403f00d98b84b1b5
DaveenDev <daveendev07@gmail.com>
2022-12-28 11:27:29 +0800

    Added error handling and gui styling

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import functions
 import PySimpleGUI as gui
+import time
 
+gui.theme("DarkBlue2")
+time_label = gui.Text('Time', key="time_label")
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter a todo", key="todo")
 add_button = gui.Button("Add")
@@ -11,14 +14,15 @@ edit_button = gui.Button("Edit")
 complete_btn = gui.Button("Complete")
 exit_button = gui.Button("Exit")
 window = gui.Window("My Todo App",
-                    layout=[[label], [input_box, add_button],
+                    layout=[[time_label],
+                            [label], [input_box, add_button],
                             [list_box, edit_button, complete_btn],
                             [exit_button]],
                     font=('Helvetica', 18))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=10)
+    window['time_label'].update(value=time.strftime("%m.%d.%Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -26,26 +30,32 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n"
 
-            todos = functions.get_todos()
-            index_to_edit = todos.index(todo_to_edit)
-            todos[index_to_edit] = new_todo
+                todos = functions.get_todos()
+                index_to_edit = todos.index(todo_to_edit)
+                todos[index_to_edit] = new_todo
 
-            functions.write_todos(todos)
+                functions.write_todos(todos)
 
-            window['todos'].update(values=todos)
+                window['todos'].update(values=todos)
+            except IndexError as error:
+                gui.popup("Please select an item to edit", font=("Helvetica", 20))
         case "Complete":
-            todo_to_complete = values['todos'][0]
+            try:
+                todo_to_complete = values['todos'][0]
 
-            todos = functions.get_todos()
-            index_to_edit = todos.index(todo_to_complete)
-            todos.remove(todo_to_complete)
+                todos = functions.get_todos()
+                index_to_edit = todos.index(todo_to_complete)
+                todos.remove(todo_to_complete)
 
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value="")
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError as error:
+                gui.popup("Please select an item to complete", font=("Helvetica", 20))
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case "Exit":
