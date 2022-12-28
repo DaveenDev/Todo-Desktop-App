ceb5709d29881b95c3ceda827368cc319acc19d5
DaveenDev <daveendev07@gmail.com>
2022-12-28 10:46:18 +0800

    Completed the feet/inches to meter  converter gui program

--- Bonus/converter.py
+++ Bonus/converter.py
@@ -1,11 +1,26 @@
 import PySimpleGUI as gui
 
-input1 = gui.InputText()
-input2 = gui.InputText()
-button = gui.Button("Conver")
-
+input1 = gui.InputText(key="feet")
+input2 = gui.InputText(key="inches")
+button = gui.Button("Convert")
+status_label = gui.Text(key="status")
 window = gui.Window("Converter",
                     layout=[[gui.Text("Enter Feet:"), input1],
                             [gui.Text("Enter inches"), input2],
-                            [button]])
-window.read()
+                            [button, status_label]])
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case "Convert":
+            feet = float(values["feet"])
+            inches = float(values["inches"])
+            total_inches = (feet * 12) + inches
+            meter = total_inches * 0.0254
+            window["status"].update(value=f"{meter} m")
+        case gui.WIN_CLOSED:
+            break
+window.close()
